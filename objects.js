8348b57bf3846f6e63f8ac42396f83c76ed2274c
modern-fork-2657 <kalkidan.solomon2@gmail.com>
2020-04-07 00:00:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ let playlist={
   nellyFurtado: "Promiscous girl",
   drake: "Jungle",
 };
+
+function updatePlaylist(playlist, artistName,songTitle){
+  return Object.assign({},playlist,{artistName:songTitle});
+};
